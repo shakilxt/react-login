376a87ab4cd23cfe8549bbf22e04fbff1d50cf63
Shakil <ai.shakil.xt@gmail.com>
2025-10-27 07:50:35 +0600

    Fix: Configure database connection for production

--- server/config/db.js
+++ server/config/db.js
@@ -3,12 +3,19 @@ import env from 'dotenv'
 
 env.config()
 
+// const db = new Pool({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_DATABASE,
+//     password: process.env.DB_PASSWORD,
+//     port: process.env.DB_PORT
+// })
+
 const db = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 })
 
 export default db
